1fefa0b3701d32b4476d41a0316087cdba33e22b
LukaszP <lukaszp4@onet.eu>
2019-07-06 17:46:41 +0200

    I am commiting the streams.

--- src/JavaStreams.java
+++ src/JavaStreams.java
@@ -2,10 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -99,8 +96,16 @@ public class JavaStreams {
                     System.out.println(key + " " + map.get(key));
                 }*/
         //13. Reduction - sum
-        double total = Stream.of(7.3,1.5,4.8)
+        /*double total = Stream.of(7.3,1.5,4.8)
                 .reduce(0.0, (Double a, Double b) -> a + b);
-        System.out.println("Total = " + total);
+        System.out.println("Total = " + total);*/
+
+        //14. Reduction - summary statistics
+        IntSummaryStatistics summaryStatistics = IntStream.of(7,2,19,88,73,4,10)
+                .summaryStatistics();
+        System.out.println(summaryStatistics);
+
+
+
         }
     }
